2d56fe189ad3e0c3f08182ab4c42f4722b2a0088
Paulo <hbr.paulo@gmail.com>
2025-07-26 19:24:11 -0300

    parameterize test label

--- R/tab_desc_num.R
+++ R/tab_desc_num.R
@@ -1,8 +1,9 @@
 #' @description Unify results for numerical variables
 #' @param aux Data frame with survival data
+#' @param test Label for the statistical test (default "-")
 #' @return A tibble with frequency and survival statistics
 
-tab_desc_num <- function(aux, column, k = 4){
+tab_desc_num <- function(aux, column, k = 4, test = '-'){
   fit <- coxph(data = aux, Surv(aux$tempos, aux$censura)~.y.)
   
   tibble(
@@ -10,5 +11,6 @@ tab_desc_num <- function(aux, column, k = 4){
     group1 = NA,
     group2 = fit %>% coef %>% exp %>% round(., k) %>% as.character, 
     p = format_sig(summary(fit)[["sctest"]][["pvalue"]]),
-    test = 'placeholder'
-  )}
+    test = test
+  )
+}
--- scripts/tab_desc_num.R
+++ scripts/tab_desc_num.R
@@ -1,14 +1,16 @@
 #' @description Unify results for numerical variables
 #' @param aux Data frame with survival data
+#' @param test Label for the statistical test (default "-")
 #' @return A tibble with frequency and survival statistics
 
-tab_desc_num <- function(aux, column, k = 4){
+tab_desc_num <- function(aux, column, k = 4, test = '-'){
   fit <- coxph(data = aux, Surv(aux$tempos, aux$censura)~.y.)
-  
+
   tibble(
     .y. = 'Regression coefficient',
     group1 = NA,
-    group2 = fit %>% coef %>% exp %>% round(., k) %>% as.character, 
+    group2 = fit %>% coef %>% exp %>% round(., k) %>% as.character,
     p = format_sig(summary(fit)[["sctest"]][["pvalue"]]),
-    test = 'placeholder'
-  )}
+    test = test
+  )
+}
