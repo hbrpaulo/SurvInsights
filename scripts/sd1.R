1ebf45dc0b980e6ae270a5058a44166d03840506
Paulo <hbr.paulo@gmail.com>
2025-07-26 19:58:27 -0300

    Remove helper scripts and update README with usage info
    Deleted several back-up R helper scripts related to descriptive survival analysis and table generation. Updated README to include instructions for sample datasets and steps to reproduce the example analysis, reflecting the removal of these scripts.

--- scripts/sd1.R
+++
@@ -1,9 +0,0 @@
-#' @description: sd1(x)
-#' This function calculates the standard deviation of a vector x.
-#' If the length of x is less than 2, it returns 0.
-#' @param x A numeric vector.
-#' @return The standard deviation of x, or 0 if x has less than 2 elements.
-#' @examples
-#' sd1(c(1, 2, 3)) #' returns the standard deviation of the vector
- 
-sd1 <- function(x){ifelse(length(x)<2, 0, sd(x))}
--- scripts/tab_desc_fac.R
+++
@@ -1,8 +0,0 @@
-#' @description Unify results for categorical variables
-#' @param aux Data frame with survival data
-#' @return A tibble with frequency and survival statistics
- 
-tab_desc_fac <- function(aux){
-  ungroup(full_join(tab_freq(aux), 
-                    msdr_y(aux), by = join_by(.y.)))
-}
