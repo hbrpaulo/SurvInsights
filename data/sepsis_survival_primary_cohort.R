68b9eb8849311bc3e079e384bb8bddce0a4324bc
Paulo <hbr.paulo@gmail.com>
2025-07-26 22:40:25 -0300

    Update package metadata and improve documentation
    Revised DESCRIPTION to reflect new package title, author, and description. Added example dataset documentation. Updated Rd files to use backticks for column names and improved clarity in argument and description sections.

---
+++ data/sepsis_survival_primary_cohort.R
@@ -0,0 +1,12 @@
+#' My Example Dataset
+#'
+#' A short description of what the dataset contains and what it's for.
+#'
+#' @format A data frame with X rows and Y variables:
+#' \describe{
+#'   \item{column1}{Description of column 1}
+#'   \item{column2}{Description of column 2}
+#'   \item{...}{...}
+#' }
+#' @source Source of the data, e.g., a URL, paper, or note saying "simulated data"
+"sepsis_survival_primary_cohort"
