b7dfcba84f7db98bfde8c664111cd9807cbb2d57
Paulo <hbr.paulo@gmail.com>
2025-08-19 18:35:58 -0300

    Add scroll option to complete_tab function
    Introduces a new 'scroll' parameter to the complete_tab function, allowing users to enable or disable the scroll_box feature. The scroll box height is set to 600px when enabled, providing more flexibility in table display.

--- R/complete_tab.R
+++ R/complete_tab.R
@@ -2,7 +2,7 @@
 #' @importFrom kableExtra kbl kable_classic row_spec column_spec scroll_box footnote
 NULL
 
-complete_tab <- function(data, time_col = "tempos", event_col = "censura"){
+complete_tab <- function(data, time_col = "tempos", event_col = "censura", scroll = TRUE){
 
   columns <- column_classifier(data) %>%
     filter(!(column %in% c(time_col, event_col, '.y.'))) %>%
@@ -14,17 +14,20 @@ complete_tab <- function(data, time_col = "tempos", event_col = "censura"){
                                                       time_col = time_col,
                                                       event_col = event_col)))
   
-  table %>%
+  table <- table %>%
     kbl('html', digits = 4, escape = FALSE, booktabs = TRUE,
         longtable = TRUE, align = 'c', decimal.mark = ',') %>%
     kable_classic %>%
     row_spec(which(table$highlight=='J1'), bold = T, color = "black",
              align = 'c', background = "grey95") %>%
     column_spec(ncol(table), color = 'white', background = 'white') %>%
-    scroll_box(height = "1000px") %>%
     footnote(general = "Here is a general comments of the table. ",
              number = c("Footnote 1; ", "Footnote 2; "),
              alphabet = c("Footnote A; ", "Footnote B; "),
              symbol = c("Footnote Symbol 1; ", "Footnote Symbol 2")
     )
+  if(scroll){
+    table <- table %>% 
+    scroll_box(height = "600px")}
+  return(table)
 }
